8a77a7d1a61bec0a50cb7c1af8aee5cb62fa5187
GitHub Action <action@github.com>
2021-09-03 03:00:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/rikotsushima/axonplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rikotsushima/axonplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rikotsushima.axonplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rikotsushima.axonplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rikotsushima/axonplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rikotsushima/axonplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rikotsushima.axonplugin.services
 
+import com.github.rikotsushima.axonplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
